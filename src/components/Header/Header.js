dd119259d2ae7ef0aa1d90ea0cb07f1a4cbcbb9b
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-09 12:01:24 +0100

    Update components
    Update components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,8 @@ import './Header.css';
 function Header() {
   return (
     <div>
-      
+        <h1>I am the Header Section</h1>
+        <h2>I will contain the navigation elements in desktop responsive and hamburger in mobile responsive</h2> 
     </div>
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Home() {
   return (
     <div>
-      
+        <h1>I am the Home Page</h1>  
     </div>
   )
 }
