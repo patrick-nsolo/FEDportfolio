fc4d62593c49ded7b00a36e63cef9ae33ecfc147
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-09 12:05:54 +0100

    Update contact components

--- src/components/ContactForm/Contact.js
+++ src/components/ContactForm/Contact.js
@@ -4,7 +4,8 @@ import './Contact.css';
 function Contact() {
   return (
     <div>
-      
+        <h1>I am the Contact Section</h1>
+        <h2>I will contain a contact form that is linked to my personal email </h2>  
     </div>
   )
 }
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -4,7 +4,8 @@ import './Video.css';
 function Video() {
   return (
     <div>
-      
+        <h1>I am the Video Section</h1>
+        <h2>I should contain videos but dont know if this is necessary </h2>  
     </div>
   )
 }
