1b781ed07231bddeae47d3d17d10807121b41a94
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-09 12:02:45 +0100

    Update project component

--- src/components/MyProjects/Projects.js
+++ src/components/MyProjects/Projects.js
@@ -4,7 +4,8 @@ import './Projects.css';
 function Projects() {
   return (
     <div>
-      
+        <h1>I am the Project Section</h1>
+        <h2>I will contain all web design projects neatly </h2>    
     </div>
   )
 }
