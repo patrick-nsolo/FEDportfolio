1cec5cf3d35ac90c0fc738d0a7eaf75299d4bd03
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-09 12:31:29 +0100

    update Header
    Creation of navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
-  return (
-    <nav className='navigation'>
-        <div className='logo'>
-            <a href='/'>Patrick Nsolo</a>
-        </div>
-        <ul className='nav-links'>
-            <li><a href='#About'>About Me</a></li>
-            <li><a href='#Projects'>My Projects</a></li>
-            <li><a href='#Contact'>Contact Me</a></li>
-        </ul>     
-    </nav>
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+    const toggleMobileMenu = () =>{
+        setShowMobileMenu(!showMobileMenu);
+    };
+    return (
+        <nav className={'navigation'}>
+            <div className='logo'>
+                <a href='/'>Patrick Nsolo</a>
+            </div>
+            <ul className='nav-links'>
+                <li><a href='#About'>About Me</a></li>
+                <li><a href='#Projects'>My Projects</a></li>
+                <li><a href='#Contact'>Contact Me</a></li>
+            </ul>     
+        </nav>
   )
 }
 
