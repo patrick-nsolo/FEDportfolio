f6aa4e922ca588937e6c7b57dfde34635b97196b
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-09 11:50:50 +0100

    update App.js
    update App.js with component imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/AboutMe/About';
+import Contact from './components/ContactForm/Contact';
+import Gallery from './components/Gallery/Gallery';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Projects from './components/MyProjects/Projects';
+import 
 
 function App() {
   return (
