1fa7a0ca8fdefbaa014b02302bd16beac3f89f8b
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-14 16:42:08 +0100

    Update React web app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Typed from 'react-typed';
 import './Home.css';
 
 function Home() {
   return (
-    <div>
-        <h1>I am the Home Page</h1>  
+    <div className="home-container">
+      <h1 className="intro">Hello! <span class="wave">👋🏾</span> I'm Patrick  </h1>
+      <h2 className="role">
+        <Typed strings={['Front End Developer/ Graphic Designer']} typeSpeed={40} backSpeed={50} loop/>
+      </h2>
+      <h2>Self taught and passionate about code and design. Always Learning.</h2>
     </div>
   )
 }
