07c82cc5c9072dc92aa3879c7ff7e1ed3cbe79ed
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-14 17:03:09 +0100

    update react web app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,9 @@ function Home() {
     <div className="home-container">
       <h1 className="intro">Hello! <span class="wave">👋🏾</span> I'm Patrick  </h1>
       <h2 className="role">
-        <Typed strings={['Front End Developer/ Graphic Designer']} typeSpeed={40} backSpeed={50} loop/>
+        <Typed strings={['Front End Developer/ Graphic Designer']} typeSpeed={40} backSpeed={50} loop={false}/>
       </h2>
-      <h2>Self taught and passionate about code and design. Always Learning.</h2>
+      <h2>Self taught, and passionate about code and design. Always Learning.</h2>
     </div>
   )
 }
