7c2ad6f0cf736c818b908e695e2e934925268e70
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-10 09:51:48 +0100

    update project section with lhc project

--- src/components/MyProjects/Projects.js
+++ src/components/MyProjects/Projects.js
@@ -25,8 +25,21 @@ function Projects() {
               <a href='https://wellnestshowroom.com/' target='_blank' rel="noreferrer">Live Page</a>
               <a href='https://github.com/patrick-nsolo/pfl-fix' target='_blank' rel="noreferrer">Code</a>
             </div>  
+          </div> 
+        </div>  
+        <div className='lhc-project'>
+          <div className='lhc-img'>
+            <img src='../images/lofty.webp'alt=''/>  
           </div>
-        </div>   
+          <div className='project-details'>
+            <h2>Paradigm Finance Limited Website</h2>
+            <p className='desription'>An e-commerce shop. Built using WordPress, HTML, CSS, and PHP</p>
+            <div className='btn-links'>
+              <a href='https://wellnestshowroom.com/' target='_blank' rel="noreferrer">Live Page</a>
+              <a href='https://github.com/patrick-nsolo/pfl-fix' target='_blank' rel="noreferrer">Code</a>
+            </div>  
+          </div> 
+        </div> 
     </div>
   )
 }
