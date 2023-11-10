dac6bce75bb3a7b00fcc2a25a9f6d4ef234ddd59
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-10 11:36:12 +0100

    update gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,25 +1,13 @@
-import React from 'react';
+import React from 'react'
 import './Gallery.css';
-import { Carousel } from 'react-responsive-carousel';
 
 const Gallery = () => {
-  const images = [
-    '../upload/product-label.webp',
-    '/upload/business-flier.webp',
-    '/upload/company-flier.webp',
-    '/upload/e-flier.webp',
-    '/upload/flier.webp',
-    '/upload/church-flier.webp',
-  ]
   return (
-    <Carousel className='gallery-bg'>
-        {images.map((image,index) => (
-          <div key={index} onClick={() => window.open(image, '_blank')}>
-            <img src={images} alt={`Image ${index}`}/>
-          </div>
-        ))}
-    </Carousel>
+    <div>
+      <h1> hello world</h1>
+    </div>
   )
 }
 
 export default Gallery
+
