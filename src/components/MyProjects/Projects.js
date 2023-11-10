bb115c254a3a2d1ca0d46808ca4923a34f2cd997
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-10 09:13:50 +0100

    update

--- src/components/MyProjects/Projects.js
+++ src/components/MyProjects/Projects.js
@@ -5,10 +5,14 @@ function Projects() {
   return (
     <div className='project-display'>
         <div className='wellnest-project'>
-          <div>
-            
+          <div className='wellnest-img'>
+            <img src='../images/wellnest.webp'alt=''/>  
           </div>
-
+          <div className='project-details'>
+            <h2>Wellnest Showroom Website</h2>
+            <p className='desription'>An e-commerce shop. Built using WordPress, HTML, CSS, and PHP</p>
+            <a href='https://wellnestshowroom.com/'>Live Page</a>
+          </div>
         </div>   
     </div>
   )
