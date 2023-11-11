edabf65734061ac54b5bf3b83ecf5b1afb48e7d0
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-11 16:43:59 +0100

    update styling

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -3,9 +3,25 @@ import './About.css';
 
 function About() {
   return (
-    <div>
-        <h1>I am the About Section</h1> 
-        <h2>I will contain information about patrick nsolo </h2>  
+    <div className='about' id='about'>
+        <h2>About Me</h2> 
+        <p>
+        I am an enthusiastic and dedicated software developer, fluent in English, 
+        with expertise in WordPress, HTML, CSS, JavaScript, and related frameworks [React]. 
+        My journey in web design has been primarily self-taught, fueled by an unwavering 
+        commitment to continual learning and improvement.
+        </p>
+        <p>
+        I have actively dedicated my time and effort to participating in several open-source 
+        projects and communities through software development, graphic design, documentation, 
+        and more.
+        </p>
+        <p>
+        In addition to my proficiency in web development, I possess advanced graphic design 
+        and video editing skills, adeptly using tools such as Photoshop, Inkscape, Figma, 
+        Davinci Resolve, and Microsoft Clipchamp. This allows me to offer a comprehensive 
+        range of services to enhance the visual aspects of your project.
+        </p>
     </div>
   )
 }
