4a4682c8bc7bf843a5933d97bd9a9140d83c9d83
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-11 17:35:21 +0100

    update

--- src/components/ContactForm/Contact.js
+++ src/components/ContactForm/Contact.js
@@ -3,10 +3,18 @@ import './Contact.css';
 
 function Contact() {
   return (
-    <div>
-        <h1>I am the Contact Section</h1>
-        <h2>I will contain a contact form that is linked to my personal email </h2>  
+    <div className='contact-container'>
+      <div className='contact' id='contact'>
+        <h2>Lets Connect!</h2>
+        <p>If you would like to reach me, Please send me a message here and follow me on social media</p>  
+        <div class="contact-links">
+            <a className='m-3' href='https://github.com/patrick-nsolo' target='_blank' rel='noreferrer'><i class="fa-brands fa-github icon"></i></a>
+            <a className='m-3' href='https://www.facebook.com/hotboi2thamost' target='_blank' rel='noreferrer'><i class="fa-brands fa-facebook icon"></i></a>
+            <a className='m-3' href='https://www.linkedin.com/in/patrick-nsolo-83b095a2/' target='_blank' rel='noreferrer'><i class="fa-brands fa-linkedin icon"></i></a>
+        </div>
+      </div>
     </div>
+    
   )
 }
 
