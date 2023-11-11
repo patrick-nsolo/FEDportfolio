ea6fa634c86a504f55d5d534865786b9ca0e5e76
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-11 14:34:54 +0100

    update nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ function Header() {
                 <div className='bar'></div>
                 <div className='bar'></div>
             </div>
-            <ul className='nav-links'>
+            <ul className={`${styles.nav-links} ${isActive ? styles.active : ''}`}>
                 <li><a href='#about'>About Me</a></li>
                 <li><a href='#projects'>My Projects</a></li>
                 <li><a href='#contact'>Contact Me</a></li>
